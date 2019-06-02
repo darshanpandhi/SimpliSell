b24a43eda466790e921bf81cc2c3e0ee7cdfd128
Renz Cabusas <cabusasr@myumanitoba.ca>
2019-06-02 09:55:14 -0500

    File structure

--- app/src/androidTest/java/com/simplisell/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/simplisell/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.simplisell;
+package com.simplisell;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.simplisell", appContext.getPackageName());
+        assertEquals("com.simplisell", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/com/simplisell/persistence/PersistenceTest.java
@@ -0,0 +1,4 @@
+package com.simplisell.persistence;
+
+public class PersistenceTest {
+}
