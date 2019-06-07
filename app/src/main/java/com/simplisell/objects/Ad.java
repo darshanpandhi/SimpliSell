99fb719faa0cb0e73887c34fb687346b5725bc97
Darshan <43501829+darshanpandhi@users.noreply.github.com>
2019-06-07 01:03:13 -0500

    Remove adDate attribut for Ad object for now

--- app/src/main/java/com/simplisell/objects/Ad.java
+++ app/src/main/java/com/simplisell/objects/Ad.java
@@ -10,7 +10,6 @@ public final class Ad
     private final UserAdvertiser adOwner;
     private final AdType adType;
     private final Category category;
-    private final Date adPostDate;
     private final String title;
     private final String description;
     private final double price;
@@ -22,15 +21,13 @@ public final class Ad
         adOwner = null;
         adType = null;
         category = null;
-        adPostDate = null;
         title = null;
         description = null;
         price = 0;
     }
 
 
-    Ad(final UserAdvertiser adOwner, final AdType adType, final Category category, final Date
-            adPostDate, final String title, final String description, final double price)
+    Ad(final UserAdvertiser adOwner, final AdType adType, final Category category, final String title, final String description, final double price)
     {
         adId = idCounter;
         idCounter++;
@@ -38,7 +35,6 @@ public final class Ad
         this.adOwner = adOwner;
         this.adType = adType;
         this.category = category;
-        this.adPostDate = adPostDate;
         this.title = title;
         this.description = description;
         this.price = price;
