25f0f5605a9c9c16161388ab178f9269959b2ee4
Darshan <43501829+darshanpandhi@users.noreply.github.com>
2019-06-07 00:42:54 -0500

    Fix bugs in UserAdvertiser.java

--- app/src/main/java/com/simplisell/objects/UserAdvertiser.java
+++ app/src/main/java/com/simplisell/objects/UserAdvertiser.java
@@ -1,5 +1,31 @@
 package com.simplisell.objects;
 
-public class UserAdvertiser
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAdvertiser extends User
 {
+    private List<Ad> postings;
+
+    public UserAdvertiser(String userName, String password)
+    {
+        super(userName, password);
+        postings = new ArrayList<>();
+    }
+
+    public void addNewAd(Ad ad)
+    {
+        this.postings.add(ad);
+    }
+
+    public void deleteAd(String adID)
+    {
+        this.postings.remove(adID);
+    }
+
+    public List<Ad> getPostings()
+    {
+        return postings;
+    }
+
 }
