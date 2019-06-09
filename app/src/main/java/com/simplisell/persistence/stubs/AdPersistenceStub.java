987ce4db3b6f8c671aa8cf9297c5738c98e3676d
Darshan <43501829+darshanpandhi@users.noreply.github.com>
2019-06-08 19:46:30 -0500

    Change users in AdPersistenceStub to reflect those in AdPersistenceStub

--- app/src/main/java/com/simplisell/persistence/stubs/AdPersistenceStub.java
+++ app/src/main/java/com/simplisell/persistence/stubs/AdPersistenceStub.java
@@ -18,26 +18,32 @@ public class AdPersistenceStub implements AdPersistence
         String adOwner = "bob";
         AdType adType = AdType.OFFERING;
         Category category = Category.ELECTRONICS;
-        String title = "Brand new 6th Generation 128 gb Apple iPad 9.7 inch";
-        String description = "Selling my iPad as I got a new 2 in 1 laptop. Hardly Used" +
-                "(15 days). Got the original packaging (boxes) and all accessories. All the items" +
-                " are still in their warranty period. Have added a picture of the warranty with" +
-                " this ad.\n" + "\n" + "I have attached a premium quality tampered glass on the " +
-                "iPad to protect the screen. I also have a blue denim cover which can hold the " +
-                "apple pencil as well.";
+        String title = "iPad 6th Gen";
+        String description = "iPad Ad description";
         double price = 554.99;
 
         Ad newAd = new Ad(adOwner, adType, category, title, description, price);
-
         ads.add(newAd);
 
-        ads.add(new Ad("bob", AdType.OFFERING, Category.ELECTRONICS, "Electronics Test 1",
-                "Description for electronics test 1", 10));
-        ads.add(new Ad("bob", AdType.OFFERING, Category.ELECTRONICS, "Electronics Test 2",
-                "Description for electronics test 2", 15));
-        ads.add(new Ad("bob", AdType.OFFERING, Category.ELECTRONICS, "Electronics Test 3",
-                "Description for electronics test 3", 20));
+        adOwner = "Allice";
+        adType = AdType.WANT;
+        category = Category.JOBS_SERVICES;
+        title = "Wanted Tutoring Services";
+        description = "Tutoring Services Description";
+        price = 40;
 
+        newAd = new Ad(adOwner, adType, category, title, description, price);
+        ads.add(newAd);
+
+        adOwner = "Jay";
+        adType = AdType.OFFERING;
+        category = Category.TRANSPORTATION;
+        title = "CarPooling Ad Title";
+        description = "CarPooling Ad Description";
+        price = 100;
+
+        newAd = new Ad(adOwner, adType, category, title, description, price);
+        ads.add(newAd);
     }
 
     public List<Ad> getAds()
