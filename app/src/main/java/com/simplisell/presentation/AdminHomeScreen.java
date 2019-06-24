34fc1dcf8d5144e10ff424ab3c8724a35b8cfa67
Darshan <43501829+darshanpandhi@users.noreply.github.com>
2019-06-23 23:08:37 -0500

    Fix Merge Conflicts and associated bug fixes

---
+++ app/src/main/java/com/simplisell/presentation/AdminHomeScreen.java
@@ -0,0 +1,17 @@
+package com.simplisell.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.simplisell.R;
+
+public class AdminHomeScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home_screen);
+    }
+}
