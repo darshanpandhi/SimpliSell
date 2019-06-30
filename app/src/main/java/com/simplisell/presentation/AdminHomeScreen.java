7eb9d8ee0fea8a5f1ee2942822e967196b25ca23
Darshan <43501829+darshanpandhi@users.noreply.github.com>
2019-06-30 17:31:10 -0500

    Complete the admin_home_screen_activity

--- app/src/main/java/com/simplisell/presentation/AdminHomeScreen.java
+++ app/src/main/java/com/simplisell/presentation/AdminHomeScreen.java
@@ -3,6 +3,7 @@ package com.simplisell.presentation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
 import com.simplisell.R;
 
 public class AdminHomeScreen extends AppCompatActivity
@@ -13,5 +14,15 @@ public class AdminHomeScreen extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_screen);
+    }
+
+    public void viewReportedUsersBtnClick(View view)
+    {
+
+    }
+
+    public void viewReportedAdsBtnClick(View view)
+    {
+
     }
 }
