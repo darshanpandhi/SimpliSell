5314b0fae8f53baa4d5299b38c7db4b923b637f2
renzcabusas <cabusasr@myumanitoba.ca>
2019-06-30 01:39:53 -0500

    Fix bug where pressing back on a new, registered user, would go back to the registration page rather than main page

--- app/src/main/java/com/simplisell/presentation/PostingAdActivity/EditAd.java
+++ app/src/main/java/com/simplisell/presentation/PostingAdActivity/EditAd.java
@@ -14,6 +14,8 @@ import com.simplisell.R;
 import com.simplisell.business.AccessAds;
 import com.simplisell.objects.Ad;
 import com.simplisell.objects.Category;
+import com.simplisell.presentation.MainActivity;
+import com.simplisell.presentation.UserProfileActivity.UserProfileMenu;
 
 public class EditAd extends AppCompatActivity
 {
@@ -151,4 +153,14 @@ public class EditAd extends AppCompatActivity
         dialog = builder.create();
         dialog.show();
     }
+
+    @Override
+    public void onBackPressed()
+    {   // if anytime the back is pressed. Go back
+
+        finish();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
 }
