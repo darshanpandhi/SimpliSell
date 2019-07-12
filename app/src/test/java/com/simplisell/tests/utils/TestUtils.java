6b2ea52b168fcc731c1af265723ad24ea22d88d5
Darshan <43501829+darshanpandhi@users.noreply.github.com>
2019-07-11 21:09:46 -0500

    Reformat code

--- app/src/test/java/com/simplisell/tests/utils/TestUtils.java
+++ app/src/test/java/com/simplisell/tests/utils/TestUtils.java
@@ -6,10 +6,13 @@ import com.simplisell.application.Main;
 import java.io.File;
 import java.io.IOException;
 
-public class TestUtils {
+public class TestUtils
+{
     private static final File DB_SRC = new File("src/main/assets/db/SS.script");
 
-    public static File copyDB() throws IOException {
+
+    public static File copyDB() throws IOException
+    {
         final File target = File.createTempFile("temp-db", ".script");
         Files.copy(DB_SRC, target);
         Main.setDBPathName(target.getAbsolutePath().replace(".script", ""));
