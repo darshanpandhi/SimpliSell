caca4bdbc946cb337f616716fad4a3116a24c91e
Renz Cabusas <cabusasr@myumanitoba.ca>
2019-07-13 19:36:29 -0500

    Fix credentials code bug

--- app/src/main/java/com/simplisell/business/Credentials.java
+++ app/src/main/java/com/simplisell/business/Credentials.java
@@ -21,7 +21,7 @@ public class Credentials
     }
 
 
-    public User authenticate(String userName, String password)
+    public User correctPassword(String userName, String password)
     {
         User user = userPersistence.getUser(userName);
 
@@ -46,52 +46,6 @@ public class Credentials
             }
         }
         return correctAnswer;
-    }
-
-
-    public boolean validPassword(final String password)
-    {
-        return !passwordHasInvalidCharacters(password) && validPasswordLength(password);
-    }
-
-
-    private boolean validPasswordLength(final String password)
-    {
-        int minPasswordSize = 6;
-        int maxPasswordSize = 12;
-        int passwordLength = password.length();
-        return passwordLength >= minPasswordSize && passwordLength <= maxPasswordSize;
-    }
-
-
-    private boolean passwordHasInvalidCharacters(final String password)
-    {
-        boolean containsInvalidCharacters = false;
-        for (int i = 0; i < password.length() && !containsInvalidCharacters; i++)
-        {
-            char character = password.charAt(i);
-            if (!isNum(character) && !isLetter(character))
-            {
-                containsInvalidCharacters = true;
-            }
-        }
-        return containsInvalidCharacters;
-    }
-
-
-    private boolean isNum(final char character)
-    {
-        int asciiForCharacter = (int) character;
-        return asciiForCharacter >= '0' && asciiForCharacter <= '9';
-    }
-
-
-    private boolean isLetter(final char character)
-    {
-        int asciiForCharacter = (int) character;
-        boolean isLowerCaseLetter = asciiForCharacter >= 'a' && asciiForCharacter <= 'z';
-        boolean isUpperCaseLetter = asciiForCharacter >= 'A' && asciiForCharacter <= 'Z';
-        return isLowerCaseLetter || isUpperCaseLetter;
     }
 
 
---
+++ app/src/main/java/com/simplisell/business/ValidPasswordChecker.java
@@ -0,0 +1,49 @@
+package com.simplisell.business;
+
+public class ValidPasswordChecker {
+
+    public static boolean validPassword(final String password)
+    {
+        return !passwordHasInvalidCharacters(password) && validPasswordLength(password);
+    }
+
+
+    private static boolean validPasswordLength(final String password)
+    {
+        int minPasswordSize = 6;
+        int maxPasswordSize = 12;
+        int passwordLength = password.length();
+        return passwordLength >= minPasswordSize && passwordLength <= maxPasswordSize;
+    }
+
+
+    private static boolean passwordHasInvalidCharacters(final String password)
+    {
+        boolean containsInvalidCharacters = false;
+        for (int i = 0; i < password.length() && !containsInvalidCharacters; i++)
+        {
+            char character = password.charAt(i);
+            if (!isNum(character) && !isLetter(character))
+            {
+                containsInvalidCharacters = true;
+            }
+        }
+        return containsInvalidCharacters;
+    }
+
+
+    private static boolean isNum(final char character)
+    {
+        int asciiForCharacter = (int) character;
+        return asciiForCharacter >= '0' && asciiForCharacter <= '9';
+    }
+
+
+    private static boolean isLetter(final char character)
+    {
+        int asciiForCharacter = (int) character;
+        boolean isLowerCaseLetter = asciiForCharacter >= 'a' && asciiForCharacter <= 'z';
+        boolean isUpperCaseLetter = asciiForCharacter >= 'A' && asciiForCharacter <= 'Z';
+        return isLowerCaseLetter || isUpperCaseLetter;
+    }
+}
